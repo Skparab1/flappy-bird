9cf25ee2111babac52b239079d200a3bcb977f1e
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-05-20 12:49:27 -0700

    Add files via upload

--- flappy bird.py
+++ flappy bird.py
@@ -2,10 +2,13 @@ import time
 def clearscreen():
     print('\n'*50)
 height = 10
+adder = 0
+obstacleabove = '                                         |   '
 while True:
     try:
         clearscreen()
-        print('_'*100), print('\n'*(16 - round(height))), print('(0)>'), print('_|_ '), print('\n'*round(height)), print('_'*100)
+        top = '\n' + obstacleabove
+        print('_'*100), print(top*(16 - round(height))), print('(0)>'), print('_|_ '), print(('\n'+ obstacleabove)*round(height)), print('_'*100)
         time.sleep(0.1)
         height -= 0.5
         if height <= -1:
@@ -14,6 +17,11 @@ while True:
         if height >= 16:
             print('you have hit the ceiling')
             break
+        obstacleabove = obstacleabove[1:100]
+        adder += 1
+        if adder >= 10:
+            adder = 0
+            obstacleabove = obstacleabove[1:90] + '              |'
     except:
         height += 2
         time.sleep(0.1)
